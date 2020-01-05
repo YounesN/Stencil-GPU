f50d16b995227099ca5e2d50d7c5131f636c63c8
Younes Nejahi <younes@wayne.edu>
2020-01-04 23:06:29 -0800

    fix?

--- Stencil/warp_shuffle/GPU.cu
+++ Stencil/warp_shuffle/GPU.cu
@@ -168,7 +168,7 @@ __global__ void run_single_stencil(int *dev_input, int *dev_output, const int C,
 
   /* Write the sum back to global memory */
   for(i=stride; i<P+stride; i++) {
-    if(lane >= 2*stride && lane+offset_x < length-stride) {
+    if(lane >= 2*stride && lane+offset_x < length) {
       dev_output[from2Dto1D(lane+offset_x-stride, i+offset_y, length)] = o[i];
     }
   }
