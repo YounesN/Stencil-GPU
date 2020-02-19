8e2c356ff864c8db3a752e2556cd0d44584dc437
Younes Nejahi <younes@wayne.edu>
2020-02-19 09:11:25 -0800

    added cooperative testing code

---
+++ Stencil/warp_shuffle_time_skewed/test_cooperative_kernel.cu
@@ -0,0 +1,15 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+using namespace std;
+
+int main() {
+  int pi=0;
+  cuDevice dev;
+  cuDeviceGet(&dev,0) // get handle to device 0
+  cuDeviceGetAttribute(&pi, CU_DEVICE_ATTRIBUTE_COOPERATIVE_LAUNCH, dev);
+
+  cout << "pi: " << pi << endl;
+
+  return 0;
+}
