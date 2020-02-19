588dab9ad45ee64171b79964b6e96109c2839058
Younes Nejahi <younes@wayne.edu>
2020-02-19 09:31:11 -0800

    missed ;

--- Stencil/warp_shuffle_time_skewed/test_cooperative_kernel.cu
+++ Stencil/warp_shuffle_time_skewed/test_cooperative_kernel.cu
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
   int pi=0;
   CUdevice dev;
-  cuDeviceGet(&dev,0) // get handle to device 0
+  cuDeviceGet(&dev,0); // get handle to device 0
   cuDeviceGetAttribute(&pi, CU_DEVICE_ATTRIBUTE_COOPERATIVE_LAUNCH, dev);
 
   cout << "pi: " << pi << endl;
