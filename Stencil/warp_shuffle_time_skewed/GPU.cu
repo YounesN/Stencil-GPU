2e228f8b52ce9cf3fe801ffad97b8077e1e18311
Younes Nejahi <younes@wayne.edu>
2020-02-26 18:19:33 -0800

    make p=2

--- Stencil/warp_shuffle_time_skewed/GPU.cu
+++ Stencil/warp_shuffle_time_skewed/GPU.cu
@@ -15,10 +15,10 @@ using namespace std;
 #define FULL_MASK 0xffffffff
 #define DATA_TYPE float
 #define NUMBER_OF_WARPS_PER_X 8
-#define P 508
+#define P 2
 #define STRIDE 2
 #define N 5        // N = 2 * STRIDE + 1
-#define C 512        // C = (N+P-1)
+#define C 6        // C = (N+P-1)
 
 __device__ bool checkArrayAccess(int x, int y, int lengthx, int lengthy, const char *file, int line) {
   
